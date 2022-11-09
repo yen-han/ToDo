751c006517cdcc201870d694a6fc73cd79728bb9
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-11-08 22:20:03 -0500

    feature: implement edit, delete functionalities

--- app.js
+++ app.js
@@ -10,32 +10,83 @@ window.addEventListener("DOMContentLoaded", function () {
        console.log(target);
 
         let ul = document.querySelector("ul");
-        if(target.type==='submit'){
+        if(target.id ==='btn-add'){
             ul.appendChild(newToDo());
         } else{
             if(target.classList.contains('to-do')){
                 var span = target.parentNode.querySelector('span');
                 span.classList.add('checked');
             } 
-            if(target.type=== 'checkbox' && target.checked === false){
+            if(target.type === 'checkbox' && target.checked === false){
                 span.classList.remove('checked');
             }
         }
+        // Remove selected to-do from the list
         if(target.classList && target.classList.contains('remove')){
-            target.parentNode.style.display = "none";
+            target.parentNode.remove();
+
+        } else if(target.classList && target.classList.contains('edit')){
+            console.log(target.parentNode.childNodes.length)
+            if(target.parentNode.childNodes.length===4||target.parentNode.childNodes.length===7){
+                prepareEditToDo(target.parentNode);
+            }
+        }else if(target.classList && target.classList.contains('complete')){
+
+                editToDo(target.parentNode);
+
         }
     });
     
-    // 'X' button for remove to-do list
+    // Create edit & remove button
     let list = document.querySelectorAll("li");
     for (i = 0; i < list.length; i++) {
-        var span = document.createElement("span");
+        var edit = document.createElement("span");
+        var pencil = document.createTextNode("\u270E");
+        edit.className = "edit";
+        edit.appendChild(pencil);
+        list[i].appendChild( edit);
+
+        var remove = document.createElement("span");
         var x = document.createTextNode("\u00D7");
-        span.className = "remove";
-        span.appendChild(x);
-        list[i].appendChild(span);
+        remove.className = "remove";
+        remove.appendChild(x);
+        list[i].appendChild(remove);
     }
 });
+
+function prepareEditToDo(target){
+    // Creat input to edit to-do
+    //<input class="btn btn-primary" type="submit" value="+">
+    var input = document.createElement("input");
+    input.classList.add("editbox", "form-control");
+    input.type="text";
+    input.id="edit1"
+    input.placeholder="Edit to-do";
+    var complete = document.createElement("input")
+    complete.classList.add("btn", "btn-primary", "complete");
+    complete.type="submit"
+    complete.value="\u2713"
+    target.appendChild(input);
+    target.appendChild(complete);
+
+}
+function editToDo(target){
+    var toEdit = document.getElementById("edit1").value;
+    console.log(toEdit);
+    if(toEdit==='') {
+        alert("Write for editing to-do!");}
+    else{
+
+        //var toEditText= document.createTextNode(toEdit);
+        let name = target.querySelector(".to-do-name")
+        name.innerText=toEdit;
+        let clearEdit = target.querySelector(".editbox");
+        let clearCom = target.querySelector(".complete");
+        clearCom.remove();
+        clearEdit.remove();
+
+    }
+}
 
 // Create/Add new to-do list
 function newToDo(){
@@ -45,30 +96,38 @@ function newToDo(){
             <span class="">OOP345-Workshop</span>
         </li>
      */
-    var li = document.createElement("li");
-    li.classList.add("list-group-item");
     var add = document.querySelector("#add").value;
-    var text = document.createTextNode(add);
-    //li.appendChild(text);
     if (add === '') {
-      alert("You must write something!");
-    } else {
-        //document.querySelector("#add").appendChild(li);
-        var input = document.createElement("input");
-        input.classList.add("to-do","form-check-input", "me-1");
-        input.type="checkbox";
-        li.appendChild(input);
+        alert("Write the to-do!");
+    } 
+    else {
+        var li = document.createElement("li");
+        li.classList.add("list-group-item");
+
+        var text = document.createTextNode(add);
+
+            var input = document.createElement("input");
+            input.classList.add("to-do","form-check-input", "me-1");
+            input.type = "checkbox";
+            li.appendChild(input);
+
+        document.querySelector("#add").value = "";
+
+        var toDoName = document.createElement("SPAN");
+        toDoName.appendChild(text);
+        li.appendChild(toDoName);
+
+        var edit = document.createElement("span");
+        var pencil = document.createTextNode("\u270E");
+        edit.className = "edit";
+        edit.appendChild(pencil);
+        li.appendChild(edit);
+
+        var remove = document.createElement("span");
+        var x = document.createTextNode("\u00D7");
+        remove.className = "remove";
+        remove.appendChild(x);
+        li.appendChild(remove);
+        return li;
     }
-    document.querySelector("#add").value = "";
-
-    var toDoName = document.createElement("SPAN");
-    toDoName.appendChild(text);
-    li.appendChild(toDoName);
-
-    var span = document.createElement("SPAN");
-    var x = document.createTextNode("\u00D7");
-    span.className = "remove";
-    span.appendChild(x);
-    li.appendChild(span);
-    return li;
 }
